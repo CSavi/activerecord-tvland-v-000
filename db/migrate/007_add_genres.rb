0a22fcb992cce32ff5159abd68a4b254c1707560
Carrie Savinell <carrielane89@gmail.com>
2018-07-03 21:23:20 +0000

    automatically backed up by learn

--- db/migrate/007_add_genres.rb
+++ db/migrate/007_add_genres.rb
@@ -1,9 +1,8 @@
 class AddGenres < ActiveRecord::Migration[4.2]
 
   def change
-    add_column :shows, :name, :string
-    add_column :shows, :day, :string 
-    add_column :shows, :season, :string 
     add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
