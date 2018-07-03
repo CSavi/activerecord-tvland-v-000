28a43da87088eeb56628e443ef531a28544fdae1
Carrie Savinell <carrielane89@gmail.com>
2018-07-03 21:30:11 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,8 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  belongs_to :network 
+  belongs_to :network
 
-  def build_network
-
+  def build_network(argument)
+    the_network = Network.create(argument)
   end
 end
